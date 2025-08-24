fa8badea79173c119bde35350015cc0d91c641df
Tanner Lehett <john.cfplus@gmail.com>
2025-08-24 05:45:10 -0700

    Add docs to transform.ts.

--- packages/core/src/capabilities/transform.ts
+++ packages/core/src/capabilities/transform.ts
@@ -13,28 +13,50 @@ import { Quat } from '../math/quat';
  *   3) renderers may lerp/slerp previous->current with alphas in [0,1]
  */
 
+/**
+ * The private symbol under which transform data is stored on object instances.
+ */
 const SYMBOL_TRANSFORM = Symbol('engine:transform');
 
 export class Transform {
-    // Local (relative to parent)
+    /* Local position (relative to parent). */
     readonly localPosition = new Vec3();
+    /* Local rotation (relative to parent). */
     readonly localRotation = new Quat();
+    /* Local scale (relative to parent). */
     readonly localScale = new Vec3(1, 1, 1);
 
-    // Previous local (for interpolation)
+    /* Previous local position (relative to parent); for interpolation. */
     readonly previousLocalPosition = new Vec3();
+    /* Previous local rotation (relative to parent); for interpolation. */
     readonly previousLocalRotation = new Quat();
+    /* Previous local scale (relative to parent); for interpolation. */
     readonly previousLocalScale = new Vec3(1, 1, 1);
 
+    /**
+     * Flag indicating whether the transform data has been modified.
+     */
     private dirty = false;
 
+    /**
+     * Mark the Transform data as modified.
+     */
     markDirty(): void {
         this.dirty = true;
     }
+
+    /**
+     * Get the flag indicating whether the Transform data has been modified.
+     * @returns Flag indicating whether the Transform data has been modified.
+     */
     isDirty(): boolean {
         return this.dirty;
     }
 
+    /**
+     * Commit the current TRS data by updating the previous data stores to match.
+     * Then set the `dirty` flag to `false.
+     */
     commit(): void {
         this.previousLocalPosition.copy(this.localPosition);
         this.previousLocalRotation.copy(this.localRotation);
@@ -42,29 +64,32 @@ export class Transform {
         this.dirty = false;
     }
 
-    // Convenience setters
+    /* Convenience setter for updating local position. */
     setLocalPosition(x: number, y: number, z: number): this {
         this.localPosition.set(x, y, z);
         this.dirty = true;
         return this;
     }
+    /* Convenience setter for updating local scale. */
     setLocalScale(x: number, y: number, z: number): this {
         this.localScale.set(x, y, z);
         this.dirty = true;
         return this;
     }
+    /* Convenience setter for updating local rotation via a Quaternion. */
     setLocalRotationQuat(x: number, y: number, z: number, w: number): this {
         this.localRotation.set(x, y, z, w).normalize();
         this.dirty = true;
         return this;
     }
+    /* Convenience setter for updating local rotation via Euler. */
     setLocalRotationEuler(yaw: number, pitch: number, roll: number): this {
         this.localRotation.copy(Quat.fromEuler(yaw, pitch, roll));
         this.dirty = true;
         return this;
     }
 
-    /** Interpolate local TRS at alpha in [0,1]. */
+    /* Interpolate local TRS at alpha in [0,1]. */
     interpolateLocal(
         alpha: number,
         out?: { position?: Vec3; rotation?: Quat; scale?: Vec3 },
@@ -81,7 +106,7 @@ export class Transform {
     }
 }
 
-/** Class decorator: attach a Transform to each instance. */
+/* Class decorator: attach a Transform to each instance. */
 export function withTransform<
     T extends abstract new (...args: any[]) => object,
 >(target: T, context: ClassDecoratorContext<T>) {
@@ -89,6 +114,8 @@ export function withTransform<
         if (!(SYMBOL_TRANSFORM in this)) {
             Object.defineProperty(this, SYMBOL_TRANSFORM, {
                 value: new Transform(),
+                // We don't want the transform data to be interacted w/ directly outside
+                // of internal library usage
                 enumerable: false,
                 configurable: false,
                 writable: false,
@@ -97,10 +124,21 @@ export function withTransform<
     });
 }
 
-/** Presence / accessors */
+/**
+ * Returns a boolean flag indicating whether the given object has a `Transform` defined.
+ * @param obj The object for which to check.
+ * @returns True if the object has a `Transform` defined; otherwise, false.
+ */
 export function hasTransform(obj: unknown): boolean {
     return !!obj && typeof obj === 'object' && SYMBOL_TRANSFORM in (obj as any);
 }
+
+/**
+ * Get the `Transform` for the given object.
+ * @param obj The object to check.
+ * @returns The object's `Transform`.
+ * @throws {Error} If the object does not have a `Transform` defined.
+ */
 export function getTransform<T extends object>(obj: T): Transform {
     const t = (obj as any)[SYMBOL_TRANSFORM] as Transform | undefined;
     if (!t)
@@ -109,6 +147,12 @@ export function getTransform<T extends object>(obj: T): Transform {
         );
     return t;
 }
+
+/**
+ * Get the `Transform` for the given object, if it exists.
+ * @param obj The object to check.
+ * @returns The object's `Transform`, if it exists; otherwise null.
+ */
 export function maybeGetTransform<T extends object>(
     obj: T,
 ): Transform | undefined {
